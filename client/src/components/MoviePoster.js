bd1c88d2b740a9beca5abeed5425ab7041de7d81
DougOConnor <github.4l2iv@simplelogin.com>
2023-04-11 17:09:24 +0200

    bf for poster aspect ratio

--- client/src/components/MoviePoster.js
+++ client/src/components/MoviePoster.js
@@ -4,7 +4,7 @@ const MoviePoster = (props) => {
 
     let content = null
     if (props.path) {
-        content = <img style={{...props.style}} src={"https://image.tmdb.org/t/p/w500/" + props.path} />
+        content = <img style={{...props.style, aspectRatio: .667}} src={"https://image.tmdb.org/t/p/w500/" + props.path} />
     } else {
         content = <img style={{...props.style}} src={"/tmdb/blank-poster.png"} />
     } 
